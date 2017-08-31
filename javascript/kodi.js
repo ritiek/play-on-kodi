1e2d33d0e2549c249217f7b9905de79570f4591a
ritiek <ritiekmalhotra123@gmail.com>
2017-08-31 21:45:49 +0530

    Raw working version

--- javascript/kodi.js
+++ javascript/kodi.js
@@ -4,6 +4,25 @@ var request = require('request');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var path = require('path');
+var os = require('os');
+
+
+function get_localip() {
+    let ifaces = os.networkInterfaces();
+    let localip = '';
+
+    Object.keys(ifaces).forEach(function (ifname) {
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+        localip += iface.address;
+        });
+    });
+
+    return localip;
+}
 
 
 function serve_directory(directory, port) {
@@ -19,22 +38,29 @@ function serve_directory(directory, port) {
 
 
 function kodi_post(network_file, server, port) {
-    //let data = '{"id":200,"jsonrpc":"2.0","method":"Player.Open","params":{"item":{"file":"' + network_file + '"}}}';
-    let data = {"id":"200","jsonrpc":"2.0","method":"Player.Open","params":{"item":{"file":"' + network_file + '"}}};
-    let response = request.post('http://' + server + ':' + port + '/jsonrpc', data);
-    console.log('file posted');
-    return response;
+    var dataString = '{"id":519,"jsonrpc":"2.0","method":"Player.Open","params":{"item":{"file":"' + network_file + '"}}}';
+
+    let options = {
+        url: 'http://' + server + ':' + port + '/jsonrpc',
+        method: 'POST',
+        body: dataString
+    };
+
+    return request(options);
 }
 
 
+//const filepath = 'test.mkv';
 const filepath = '/home/ritiek/Videos/TV Shows/Tsuki Ga Kirei [1080p]/[HorribleSubs] Tsuki ga Kirei - 01 [1080p].mkv'
+const localip = get_localip();
+const port = '15000';
 
 const directory = path.dirname(filepath);
-console.log(directory);
-const network_file = path.basename(filepath);
+const network_file = 'http://' + localip + ':' + port + '/' + encodeURIComponent(path.basename(filepath));
 console.log(network_file);
 
-serve_directory(directory, port=15000);
+serve_directory(directory, port);
 
-setTimeout(kodi_post, 5000, network_file, 'localhost', '8050');
-//response = request.post('http://service.com/upload', {form:{key:'value'}})
+//setTimeout(kodi_post, 5000, network_file, 'localhost', '8050');
+const response = kodi_post(network_file, 'localhost', '8050');
+console.log(response.body);
