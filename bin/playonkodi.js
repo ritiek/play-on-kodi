7a8db8f9f4833846477ec208a42ac772a481ea0a
ritiek <ritiekmalhotra123@gmail.com>
2017-09-09 22:14:47 +0530

    Add serve-static as a dependency

--- bin/playonkodi.js
+++ bin/playonkodi.js
@@ -12,7 +12,7 @@ var os = require('os');
 
 function parse_args() {
     var parser = new ArgumentParser({
-        version: '0.1.8',
+        version: '0.1.9',
         addHelp:true,
         description: 'Stream your local/network content directly on Kodi.',
     });
